281d4f48dbe14dbef564c88be6ebaf377f22e99d
Oussama <oussama@jaaouani.com>
2025-11-02 14:47:09 -0500

    upgrade error handling

--- apps/gateway/src/authentication/route.ts
+++ apps/gateway/src/authentication/route.ts
@@ -1,14 +1,21 @@
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
-import { Container } from "@/core";
-import type { AuthenticationService } from "./service";
-import { createSchema, loginSchema, logoutSchema, refreshSchema } from "./validator";
+import { Container } from "@/core/index.js";
+import type { AuthenticationService } from "./service.js";
+import {
+  createSchema,
+  loginSchema,
+  logoutSchema,
+  refreshSchema,
+} from "./validator.js";
 
 export const routes = new Hono();
 
 routes.post("/register", zValidator("json", createSchema), async (context) => {
   const { username, password } = context.req.valid("json");
-  const authService = Container.resolve<AuthenticationService>("AuthenticationService");
+  const authService = Container.resolve<AuthenticationService>(
+    "AuthenticationService"
+  );
   const result = await authService.register(username, password);
 
   return context.json({
@@ -20,7 +27,9 @@ routes.post("/register", zValidator("json", createSchema), async (context) => {
 
 routes.post("/login", zValidator("json", loginSchema), async (context) => {
   const { username, password } = context.req.valid("json");
-  const authService = Container.resolve<AuthenticationService>("AuthenticationService");
+  const authService = Container.resolve<AuthenticationService>(
+    "AuthenticationService"
+  );
   const result = await authService.login(username, password);
 
   return context.json({
@@ -32,7 +41,9 @@ routes.post("/login", zValidator("json", loginSchema), async (context) => {
 
 routes.post("/refresh", zValidator("json", refreshSchema), async (context) => {
   const { token } = context.req.valid("json");
-  const authService = Container.resolve<AuthenticationService>("AuthenticationService");
+  const authService = Container.resolve<AuthenticationService>(
+    "AuthenticationService"
+  );
   const result = await authService.refresh(token);
 
   return context.json({
@@ -44,7 +55,9 @@ routes.post("/refresh", zValidator("json", refreshSchema), async (context) => {
 
 routes.post("/logout", zValidator("json", logoutSchema), async (context) => {
   const { token } = context.req.valid("json");
-  const authService = Container.resolve<AuthenticationService>("AuthenticationService");
+  const authService = Container.resolve<AuthenticationService>(
+    "AuthenticationService"
+  );
   await authService.signout(token);
 
   return context.json({ success: true });
